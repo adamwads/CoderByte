a541f29ab3f7e883429fd9247f092233192d5f34
roaster9 <roaster9@gmail.com>
2023-08-22 07:56:49 -0230

    got AlphabetSoup.js working on mingw with node & hardcoded str

--- Easy/AlphabetSoup.js
+++ Easy/AlphabetSoup.js
@@ -1,3 +1,21 @@
+// using node-v14.17.6-x64.msi & mingw, etc.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a string: ', function(input) {
+  // Call your AlphabetSoup function here, passing the input
+  const result = AlphabetSoup(input);
+  console.log(result);
+
+  rl.close();
+});
+
+
 /*
 QUESTION:
 Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
@@ -7,4 +25,4 @@ function AlphabetSoup(str) {
 }
 
 // keep this function call here
-AlphabetSoup(readline());
+AlphabetSoup("hello world");
